9aeda59fe9c77a28a44d25a447b4f1179abb7d5d
Atif Ali <atifshoukatali@yahoo.com>
2025-07-02 21:53:09 -0700

    fix overflow

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,7 @@
 function Editor(props) {
 
     return (
-        <div className="flex flex-col bg-base-100 rounded-box shadow-xl/30 p-4 overflow-auto">
+        <div className="flex flex-col bg-base-100 rounded-box shadow-xl/30 p-4">
             <div className="text-lg font-bold mb-2">Editor</div>
             <textarea
                 className="textarea textarea-bordered h-full w-full resize-none font-mono"
