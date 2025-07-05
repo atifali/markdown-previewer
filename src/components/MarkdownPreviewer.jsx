92020ab36b6cfa50d46033ad06964d02aff147b6
Atif Ali <atifshoukatali@yahoo.com>
2025-07-05 12:22:08 -0700

    include sync scroll checkbox

--- src/components/MarkdownPreviewer.jsx
+++ src/components/MarkdownPreviewer.jsx
@@ -8,12 +8,13 @@ import Footer from "./Footer";
 function MarkdownPreviewer() {
     const [markdown, setMarkdown] = useState(syntaxGuide);
     const [viewMode, setViewMode] = useState("both");
+    const [syncScroll, setSyncScroll] = useState(true);
     const contentRef = useRef(null);
     const editorTextareaRef = useRef(null);
     const previewDivRef = useRef(null);
 
     const handleEditorScroll = () => {
-        if (previewDivRef.current) {
+        if (previewDivRef.current && syncScroll) {
             const { scrollTop, scrollHeight, clientHeight } = editorTextareaRef.current;
             const scrollRatio = scrollTop / (scrollHeight - clientHeight);
             previewDivRef.current.scrollTop = scrollRatio * (previewDivRef.current.scrollHeight - previewDivRef.current.clientHeight);
@@ -21,7 +22,7 @@ function MarkdownPreviewer() {
     };
 
     const handlePreviewScroll = () => {
-        if (editorTextareaRef.current) {
+        if (editorTextareaRef.current && syncScroll) {
             const { scrollTop, scrollHeight, clientHeight } = previewDivRef.current;
             const scrollRatio = scrollTop / (scrollHeight - clientHeight);
             editorTextareaRef.current.scrollTop = scrollRatio * (editorTextareaRef.current.scrollHeight - editorTextareaRef.current.clientHeight);
@@ -41,7 +42,7 @@ function MarkdownPreviewer() {
             <Navbar />
             <div ref={contentRef} className="grid md:grid-cols-2 gap-6 h-[85vh] grow">
                 {((viewMode === "editor") || (viewMode === "both")) && < Editor markdown={markdown} setMarkdown={setMarkdown} editorTextareaRef={editorTextareaRef} handleEditorScroll={handleEditorScroll} />}
-                {((viewMode === "preview") || (viewMode === "both")) && <Previewer markdown={markdown} previewDivRef={previewDivRef} handlePreviewScroll={handlePreviewScroll} />}
+                {((viewMode === "preview") || (viewMode === "both")) && <Previewer markdown={markdown} previewDivRef={previewDivRef} handlePreviewScroll={handlePreviewScroll} syncScroll={syncScroll} setSyncScroll={setSyncScroll} />}
             </div>
             <Footer viewMode={viewMode} setViewMode={setViewMode} />
         </div>
--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
@@ -7,11 +8,27 @@ import rehypeRaw from "rehype-raw";
 import 'katex/dist/katex.min.css';
 
 function Previewer(props) {
+    const handleSyncscrollChange = (event) => {
+        props.setSyncScroll(event.target.checked);
+    };
+
     return (
         <div className="flex flex-col bg-base-100 rounded-box shadow-xl/30 p-4 overflow-auto">
             <div className="text-lg font-bold mb-2">Preview</div>
+            <div>
+                <input
+                    type="checkbox"
+                    checked={props.syncScroll}
+                    onChange={handleSyncscrollChange}
+                />
+                <label>Sync Scroll</label>
+            </div>
             <div ref={props.previewDivRef} onScroll={props.handlePreviewScroll} className="prose max-w-none dark:prose-invert markdown overflow-auto">
-                <ReactMarkdown remarkPlugins={[[remarkGfm, { singleTilde: false }], remarkMath, remarkEmoji]} rehypePlugins={[rehypeKatex, rehypeRaw]} >{props.markdown}</ReactMarkdown>
+                <ReactMarkdown
+                    remarkPlugins={[[remarkGfm, { singleTilde: false }], remarkMath, remarkEmoji]}
+                    rehypePlugins={[rehypeKatex, rehypeRaw]} >
+                    {props.markdown}
+                </ReactMarkdown>
             </div>
         </div>
     )
